f6c89b595bcc6386a76ddfa03287ac4b2d79e5b5
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-20 08:41:38 -0700

    user search component now displays a list of repos after a user searches for a github username

--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserLookupService } from './../github-service/user-lookup.service';
 import { UserData } from './../user-data.model';
+import { Repo } from './../repo.model';
 
 @Component({
   selector: 'app-user-search',
@@ -21,23 +22,42 @@ export class UserSearchComponent implements OnInit {
 
   lookupUser(username: string): void {
     this.userSearch.getUserDetails(username).subscribe(data => {
-      const body = data.json();
+      const user = data.json();
+
+      let repos = [];
+      this.userSearch.call(user.repos_url).subscribe(data => {
+        const repositories = data.json();
+        repositories.forEach(repo => {
+          repos.push(new Repo(repo.name,
+                              repo.html_url,
+                              repo.language,
+                              repo.stargazers_count,
+                              repo.homepage
+                            ));
+        });
+      });
+
+      let starredRepos = [];
+      this.userSearch.call(user.starred_url).subscribe(data => {
+        console.log(data);
+      });
+
       this.searchedUser = new UserData(
-        body.login,
-        body.name,
-        body.html_url,
-        body.avatar_url,
-        body.bio,
-        body.location,
-        body.created_at,
-        body.updated_at,
-        body.public_repos,
-        [], //this needs to be an array of repositories
-        body.followers,
-        body.following,
+        user.login,
+        user.name,
+        user.html_url,
+        user.avatar_url,
+        user.bio,
+        user.location,
+        user.created_at,
+        user.updated_at,
+        user.public_repos,
+        repos,
+        user.followers,
+        user.following,
         [] //this needs to be an array of repositories
       );
-      console.log(this.searchedUser);
+      console.log(user);
     });
   }
 }

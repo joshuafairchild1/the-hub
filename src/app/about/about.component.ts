65da1df7060fd0881da030f217b8988da26f901d
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-25 14:31:31 -0700

    fix bug that was causing the navbar to disappear when an authorized user would refresh the about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
 import * as firebase from 'firebase/app';
+import { AuthenticationService } from './../authentication/authentication.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  providers: [AuthenticationService]
 })
 export class AboutComponent {
-  loggedInUser: any;
+  loggedInUser: firebase.User;
 
-  constructor() {}
+  constructor(
+    private authService: AuthenticationService
+  ) {}
 
-  ngDoCheck(): void {
+  ngOnInit(): void {
     this.loggedInUser = firebase.auth().currentUser;
-    console.log(this.loggedInUser)
+    // console.log(this.loggedInUser)
+    // console.log(this.loggedInUser)
+    this.authService.user.subscribe(data => this.loggedInUser = data);
   }
 
 }

38ce9c8943f288997838a5b35d34b92b99c14b4d
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-24 21:25:36 -0700

    fix buggerino

--- src/app/inbox.model.ts
+++ src/app/inbox.model.ts
@@ -1,4 +1,4 @@
-import { Message } from '../message.model';
+import { Message } from './message.model';
 
 export class Inbox {
   constructor(public senderId: string, public receiverIds: string[], public threads: Message[]) {}
